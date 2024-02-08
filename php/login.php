27d7e484dab8c481298736f9443a59afc49a5f75
Neha <nehaputhran3108@gmail.com>
2024-02-08 14:09:44 +0530

    login rediredteddd

--- php/login.php
+++ php/login.php
@@ -17,7 +17,7 @@
                 if(password_verify($password, $row['Password'])){
                     $_SESSION['user'] = $row;
                     // echo $_SESSION['user'];
-                    header("Location: ../components/pages/home.php");
+                    header("Location: ../components/pages/home.html");
                     exit();
                 } else {
                     echo "Email or Password is Incorrect";
