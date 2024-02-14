165df9a641abb1070ba5378f3dd2556bde23d628
NishalNT <nishalnthingalaya1@gmail.com>
2024-02-14 12:33:28 +0530

    Revert "N"
    This reverts commit 6387b95863679fe28f4a11fef97a24de46933998.

--- php/home.php
+++ php/home.php
@@ -20,7 +20,7 @@ $userDetails = mysqli_fetch_assoc($result);
 // Check if user details exist
 if ($userDetails) {
   $profileImage = $userDetails['ProfileImage'];
-
+  
   $firstName = $userDetails['FirstName'];
   $lastName = $userDetails['LastName'];
   $position =$userDetails['Position'];
@@ -90,7 +90,7 @@ $postresult = mysqli_query($conn, $postquery);
       <div class="profile-options">
         <img src="./uploads/<?php echo $profileImage ?>" alt="Your Name" class="profile"/>
         <h2>
-          <?php echo $userDetails['FirstName'] . ' ' . $userDetails['LastName'] ?>
+          <?php echo $userDetails['FirstName'].' '.$userDetails['LastName'] ?>
         </h2>
         <p><?php echo $position ?>
         <button><a href="./profile.php">View Profile</a></button>
@@ -103,29 +103,16 @@ $postresult = mysqli_query($conn, $postquery);
         <h2> start a new post</h2>
         <button><a style="text-decoration:none; color:#fff; font-weight:600;" href="./post.php">Post</a></button>
       </div>
-      <script>
-        // Wait for the DOM to be fully loaded
-        document.addEventListener("DOMContentLoaded", function() {
-          var users = document.querySelectorAll('.user');
-
-          users.forEach(function(user) {
-            user.addEventListener('click', function() {
-              var userId = this.getAttribute('data-userid');
-              window.location.href = './profile.php?user_id=' + userId;
-            });
-          });
-        });
-      </script>
       <div class="feeds">
         <?php while ($postDetails = mysqli_fetch_assoc($postresult)) { ?>
           <div class="feed">
             <div class="feed-top">
-              <div class="user" id="user" data-userid="<?php echo $postDetails['UserID']; ?>">
+              <div class="user">
                 <div class="profile-picture">
                   <img src="./uploads/<?php echo $postDetails['ProfileImage']; ?>" alt="" class="profile">
                 </div>
                 <div class="info">
-                  <h3><?php echo $postDetails['FirstName'] . ' ' . $postDetails['LastName'] ?></h3>
+                  <h3><?php echo $postDetails['FirstName'].' '.$postDetails['LastName'] ?></h3>
                 </div>
               </div>
             </div>
@@ -164,11 +151,11 @@ $postresult = mysqli_query($conn, $postquery);
         }
       </script> -->
 
-
+      
       <div class="messaging-options" id="messaging-options">
         <h2>Messaging</h2>
         <div class="message" id="chatlist">
-
+          
         </div>
       </div>
     </section>
--- php/profile.php
+++ php/profile.php
@@ -64,18 +64,14 @@
 
 <p> From <?php echo $nativeplace ?></p>
 <p>1 connections</p>
-<<<<<<< HEAD
 <br>
 <?php if(!$viewingOwnProfile) { ?>
 <button class="connect" id="connect"><i class="fa-solid fa-user-plus"></i>  Connect</button>
+<?php } ?>
 <a href="./messages.php" class="message_btn" id="message_btn"><i class="fa-solid fa-paper-plane"></i>  Message</a>
-<?php } ?>
-<?php if($viewingOwnProfile) { ?>
-<a href="#" class="view-connect" id="view-connect"><i class="fa-solid fa-user-plus"></i>  View Connections</a>
-<?php } ?>
 </div>
 
-<br><br><br><hr>
+<br><br><hr>
 <h4><?php echo $bio ?></h4>
 <?php echo $email ?> <br>
 <?php echo $company ?> <br>
@@ -86,7 +82,6 @@
 <svg xmlns="http://www.w3.org/2000/svg" width="1.25em" height="1em" viewBox="0 0 640 512"><path fill="currentColor" d="M337.8 5.4c-10.8-7.2-24.8-7.2-35.6 0L166.3 96H48c-26.5 0-48 21.5-48 48v320c0 26.5 21.5 48 48 48h208v-96c0-35.3 28.7-64 64-64s64 28.7 64 64v96h208c26.5 0 48-21.5 48-48V144c0-26.5-21.5-48-48-48H473.7zM96 192h32c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H96c-8.8 0-16-7.2-16-16v-64c0-8.8 7.2-16 16-16m400 16c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16h-32c-8.8 0-16-7.2-16-16zM96 320h32c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H96c-8.8 0-16-7.2-16-16v-64c0-8.8 7.2-16 16-16m400 16c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16h-32c-8.8 0-16-7.2-16-16zM232 176a88 88 0 1 1 176 0a88 88 0 1 1-176 0m88-48c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16h-16v-16c0-8.8-7.2-16-16-16"/></svg>
 <?php echo $degree ?> <br>
 <?php echo $graduationyear ?> <br></div>
-=======
 <!-- <button class="connect" id="connect"><i class="fa-solid fa-user-plus"></i>  Connect</button> -->
  <a href="./messages.php" class="message_btn" id="message_btn"><i class="fa-solid fa-paper-plane"></i>  Message</a>
 </div>
@@ -120,7 +115,6 @@
 
 
   </div>
->>>>>>> ed97525bbc7ed1816b90794ccbb639f377383844
 <div class="side-profile">
     <div class="follow-section">
         <img src="../components/images/profile.jpg" alt="" class="follow-profile">
