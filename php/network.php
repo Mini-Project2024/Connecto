07e04968a5c56815f0335084dfd1039e95c36352
NishalNT <nishalnthingalaya1@gmail.com>
2024-02-16 22:11:25 +0530

    my network page done

--- php/network.php
+++ php/network.php
@@ -1,4 +1,3 @@
-
 <?php
 // session_start();
 include_once("./functions.php");
@@ -13,57 +12,37 @@ if (!isset($_SESSION['user'])) {
 
 // Access the user data from session
 $user = $_SESSION['user'];
+$current_user_id = $_SESSION['user']['UserID'];
 
-$query = "SELECT * FROM users WHERE UserID = " . $user['UserID']; // Assuming user_details table stores additional user information
+$query = "SELECT u.* FROM connections c JOIN users u ON c.user_id = u.UserID WHERE c.connector_id = $current_user_id";
 $result = mysqli_query($conn, $query);
-$userDetails = mysqli_fetch_assoc($result);
-
-// Check if user details exist
-if ($userDetails) {
-    $profileImage = $userDetails['ProfileImage'];
-    $firstName = $userDetails['FirstName'];
-    $lastName = $userDetails['LastName'];
-    $email = $userDetails['Email'];
-    $bio = $userDetails['Bio'];
-    $company = $userDetails['CompanyName'];
-    $position = $userDetails['Position'];
-    $institution = $userDetails['Institution'];
-    $degree = $userDetails['Degree'];
-    $field_of_study = $userDetails['FieldOfStudy'];
-    $graduationyear = $userDetails['GraduationYear'];
-    $coverimage = $userDetails['CoverPhotoURL'];
-    $NativePlace = $userDetails['NativePlace'];
-}
-
-$postquery = "SELECT p.*, u.* 
-              FROM posts p 
-              JOIN users u ON p.UserID = u.UserID ORDER BY p.PostedDate DESC";
-$postresult = mysqli_query($conn, $postquery);
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Network</title>
-    <link rel="stylesheet" href="../components/css/style.css">
-<style>
-   
- </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My Network</title>
+  <link rel="stylesheet" href="../components/css/style.css">
+  <style>
+
+  </style>
 
 
 </head>
 
 <body>
-<header>
+  <header>
     <div class="container">
       <div class="logo">
         <img src="../components/images/logo.png" alt="">
       </div>
       <nav>
         <ul>
-          <li><a href="./home.php">Home</a></li>
+          <li><a href="#">Home</a></li>
           <li><a href="#">My Network</a></li>
           <li><a href="#">Jobs</a></li>
           <li><a href="#">Messaging</a></li>
@@ -72,15 +51,35 @@ $postresult = mysqli_query($conn, $postquery);
     </div>
   </header>
   <br><br><br><br><br>
-  <center> <h1>Your Connections</h1></center>
- 
-  <div class="cards">
-  <img src="./uploads/<?php echo $coverimage ?>" alt="Your Name" class="connect-cover" />
-      <img src="./uploads/<?php echo $profileImage ?>" alt="Your Name" class="connect-profile" />
-      <div class="connect-content">
-      <p><?php echo $position ?> at <?php echo $company ?></p>
-      <div > <button>View Profile</button></div>
+  <center>
+    <h1>Your Connections</h1>
+  </center>
+  <script>
+    function redirectToProfile(userID) {
+      var profileUrl = "./profile.php?userID=" + userID;
+      window.location.href = profileUrl;
+    }
+  </script>
+
+  <div class="network-container">
+    <?php
+    while ($userDetails = mysqli_fetch_assoc($result)) {
+      $coverImage = $userDetails['CoverPhotoURL'];
+      $profileImage = $userDetails['ProfileImage'];
+      $position = $userDetails['Position'];
+      $company = $userDetails['CompanyName'];
+      $userId = $userDetails['UserID'];
+    ?>
+      <div class="cards">
+        <img src="./uploads/<?php echo $coverImage ?>" alt="Your Name" class="connect-cover" />
+        <img src="./uploads/<?php echo $profileImage ?>" alt="Your Name" class="connect-profile" />
+        <div class="connect-content">
+          <p><?php echo $position ?> at <?php echo $company ?></p>
+          <div> <button onclick="redirectToProfile(<?php echo $userId ?>)">View Profile</button></div>
+        </div>
       </div>
+    <?php } ?>
   </div>
 </body>
+
 </html>
--- php/profile.php
+++ php/profile.php
@@ -35,8 +35,8 @@ if ($userDetails) {
   $institution = $userDetails['Institution'];
   $degree = $userDetails['Degree'];
   $field_of_study = $userDetails['FieldOfStudy'];
-  $graduationyear = $userDetails['GraduationYear'];
-  $coverimage = $userDetails['CoverPhotoURL'];
+  $graduationYear = $userDetails['GraduationYear'];
+  $coverImage = $userDetails['CoverPhotoURL'];
   $NativePlace = $userDetails['NativePlace'];
 }
 
@@ -130,7 +130,7 @@ $no = mysqli_fetch_array($res);
   <div class="full-profile">
     <div class="main-profile">
 
-      <img src="./uploads/<?php echo $coverimage ?>" alt="Your Name" class="cover-photo" />
+      <img src="./uploads/<?php echo $coverImage ?>" alt="Your Name" class="cover-photo" />
       <img src="./uploads/<?php echo $profileImage ?>" alt="Your Name" class="profile-image" />
       <div class="username">
         <h1><?php echo $firstName . ' ' . $lastName; ?></h1>
@@ -196,7 +196,7 @@ $no = mysqli_fetch_array($res);
         <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24">
           <path fill="currentColor" d="M5 13.18v2.81c0 .73.4 1.41 1.04 1.76l5 2.73c.6.33 1.32.33 1.92 0l5-2.73c.64-.35 1.04-1.03 1.04-1.76v-2.81l-6.04 3.3c-.6.33-1.32.33-1.92 0zm6.04-9.66l-8.43 4.6c-.69.38-.69 1.38 0 1.76l8.43 4.6c.6.33 1.32.33 1.92 0L21 10.09V16c0 .55.45 1 1 1s1-.45 1-1V9.59c0-.37-.2-.7-.52-.88l-9.52-5.19a2.04 2.04 0 0 0-1.92 0" />
         </svg>
-        <p><?php echo $degree ?> in <?php echo $field_of_study ?> completed in the year <?php echo $graduationyear ?> <br> </p>
+        <p><?php echo $degree ?> in <?php echo $field_of_study ?> completed in the year <?php echo $graduationYear ?> <br> </p>
         <br>
       </div>
     </div>
